97a351cf61e895148de2314830078c78bb146f6f
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2019-12-26 22:04:08 +0000

    printing ASCII values

--- 01 Fundamentals/01 Introduction/hello.c
+++ 01 Fundamentals/01 Introduction/hello.c
@@ -1,8 +1,26 @@
+/************************
+   build with:
+     gcc hello.c -o hello
+   run with:
+     ./hello
+*************************/
+
 #include <stdio.h>
 
 int main() {
-   // learn the build cylce
-   printf("Hello, World!\n\n");
-   return 0;
+  // learn the build cylce 
+  char c;
+  printf("Hello, Mate!\n\n");
+
+  printf("Enter a character: \n");
+  printf("^C or 'stop' to stop \n");
+
+  // display the integer value of a character
+  // and the actual character
+  while (scanf("%c", &c) != EOF)
+    if (c != '\n')
+      printf("ASCII value of %c = %x\nanother character (or ^C): ", c, c);
+
+  return 0;
 }
 
