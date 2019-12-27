9d0c655000b1da61c75ee6cff1689a63ee4ae27f
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2019-12-27 07:25:21 +0000

    be aware of the repl.it bugs!

--- main.sh
+++ main.sh
@@ -14,11 +14,14 @@ hello=$PWD/"01 Fundamentals/01 Introduction"
 gcd=$PWD/"01 Fundamentals/02 C"
 arrays=$PWD/"01 Fundamentals/03 Elementary Data Structures"
 
+echo "building: "$hello
+echo "building: "$arrays
 echo "building: "$gcd
+
 echo "output files in ./Results"
 echo
 
 gcc "$hello/"hello.c -o ./Results/hello
 gcc "$gcd/"gcd.c -o ./Results/gcd
 gcc "$arrays/"arrays.c -o ./Results/arrays
-./Results/gcd
+#./Results/gcd
