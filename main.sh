1d2e5e61f5a9f8ec29819fa707406d3772d10e1a
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-01-01 13:17:45 +0000

    added listed list

--- main.sh
+++ main.sh
@@ -13,17 +13,20 @@ echo
 hello=$PWD/"01 Fundamentals/01 Introduction"
 gcd=$PWD/"01 Fundamentals/02 C"
 sieve=$PWD/"01 Fundamentals/03 Elementary Data Structures"
+list=$PWD/"01 Fundamentals/03 Elementary Data Structures"
 
 echo "building: "
 echo '+------------------+'
 echo "hello.c"
 echo "sieve.c"
 echo "gcd.c"
+echo "list.c"
 echo '+------------------+'
 echo "binaries in ./Results"
 echo '+------------------+'
 
 gcc "$hello/"hello.c -o ./Results/hello
 gcc "$gcd/"gcd.c -o ./Results/gcd
+gcc "$list/"list.c -o ./Results/list
 gcc "$sieve/"sieve.c -o ./Results/sieve
 #./Results/gcd
